a6685c60bd5c03b29a4a9435bde4a43b2d2b09e0
72PikaChu72 <72vladvlad72@gmail.com>
2023-10-31 02:00:35 +0500

    123

--- SanicApp.py
+++ SanicApp.py
@@ -2,7 +2,7 @@ from sanic import Sanic, response, HTTPResponse, json, redirect, html, file
 from sanic import Sanic
 from sanic.response import text, html
 from jinja2 import Environment, FileSystemLoader, select_autoescape
-app = Sanic("on-wifi")
+app = Sanic("WinterCRM")
 
 
 @app.route("/")
@@ -10,4 +10,4 @@ async def index(request):
     return response.text('Hello')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=8000, debug=True)
