8a268778265eb660eea832a6ef2c8d1c759e3d87
72PikaChu72 <72vladvlad72@gmail.com>
2023-11-01 21:41:34 +0500

    asdasd

--- SanicApp.py
+++ SanicApp.py
@@ -3,11 +3,14 @@ from sanic import Sanic
 from sanic.response import text, html
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from Database import Database
+
 app = Sanic("WinterCRM")
+
 env = Environment(
     loader=FileSystemLoader('temp'),  # Папка с шаблонами
     autoescape=select_autoescape(['html', 'xml'])
 )
+
 app.static("/static/", "./st/")
 
 @app.route("/")
@@ -45,11 +48,23 @@ async def schedule(request):
 async def schedule_post(request):
     return text('POST метод')
 
+@app.get('/inventory')
+async def inventoryPage(request):
+    Data = {}
+    inventory = Database.getInventory()
+    if inventory:
+        Data['Inventory'] = inventory
+    Data['InventoryTypes'] = Database.getInventoryTypes()
+    template = env.get_template('inventory.html')
+    return response.html(template.render(data = Data))
 
-@app.route("/inventory")
-async def inventory(request):
-    return response.text('Hello')
-
+@app.post('/inventory')
+async def add_inventory(request):
+    name = request.form.get('name')
+    type = request.form.get('type')
+    rented = request.form.get('rented')
+    size = request.form.get('size')
+    Database.addInventory(name,type,rented,size)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
