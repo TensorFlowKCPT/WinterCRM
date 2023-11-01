8311e77d2f8d79ccb0b753601409aa89b90dc517
Vlados <vladik00848@gmail.com>
2023-11-01 21:58:30 +0500

    1

--- SanicApp.py
+++ SanicApp.py
@@ -3,14 +3,12 @@ from sanic import Sanic
 from sanic.response import text, html
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from Database import Database
-
+import xml.etree.ElementTree as ET
 app = Sanic("WinterCRM")
-
 env = Environment(
     loader=FileSystemLoader('temp'),  # Папка с шаблонами
     autoescape=select_autoescape(['html', 'xml'])
 )
-
 app.static("/static/", "./st/")
 
 @app.route("/")
@@ -44,12 +42,36 @@ async def schedule(request):
 @app.route('/save', methods=['POST'])
 async def save(request):
     html_table = request.form.get('table')  # Получаем данные таблицы из формы
-    print(html_table)
-    return text("кайф")
+    listdate = {
+    "january": "01",
+    "february": "02",
+    "march": "03",
+    "april": "04",
+    "may": "05",
+    "june": "06",
+    "july": "07",
+    "august": "08",
+    "september": "09",
+    "october": "10",
+    "november": "11",
+    "december": "12"
+    }
 
-@app.route('/schedule', methods=['POST'])
-async def schedule_post(request):
-    return text('POST метод')
+    xmlpars = ET.fromstring(html_table)
+    nameEmployees = xmlpars.find(".//h1")
+    idEployees = Database.getIdEployee(nameEmployees.text)
+    nameMonth = xmlpars.findall(".//h2")
+    monthObject = xmlpars.findall(".//table")
+
+    for i in monthObject:
+        for j in i.findall(".//td"):
+            if j.get("style") != None and "red" in j.get("style"):
+                Database.putSchedule(idEployees, f"{i.get("id").split('_')[1]}-{listdate[i.get("id").split('_')[0]]}-{j.text}", 2)
+            if j.get("style") != None and "blue" in j.get("style"):
+                Database.putSchedule(idEployees, f"{i.get("id").split('_')[1]}-{listdate[i.get("id").split('_')[0]]}-{j.text}", 1)
+            if j.get("style") != None and "green" in j.get("style"):
+                Database.putSchedule(idEployees, f"{i.get("id").split('_')[1]}-{listdate[i.get("id").split('_')[0]]}-{j.text}", 3)
+    return response.text("успех")
 
 @app.get('/inventory')
 async def inventoryPage(request):
@@ -68,6 +90,7 @@ async def add_inventory(request):
     rented = request.form.get('rented')
     size = request.form.get('size')
     Database.addInventory(name,type,rented,size)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
