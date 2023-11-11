e40688146de55f4fa6c2260f69e066d98105215c
Arondhan <lifecrusher7@gmail.com>
2023-11-11 13:19:32 +0500

    Инвентарь(фикс бэка нужен чтоб удалять из модалки)

--- st/js/inventory.js
+++ st/js/inventory.js
@@ -1,119 +1,160 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var modal = document.getElementById('myModal');
-    var openModalButton = document.getElementById('openModalBtn');
-    var closeModalSpan = document.getElementsByClassName('svg-cross')[0];
-  
-    openModalButton.onclick = function () {
-      modal.style.display = 'block';
-    };
-  
-    closeModalSpan.onclick = function () {
-      modal.style.display = 'none';
-    };
+document.getElementById("openModalButton").addEventListener("click", function() {
+    document.querySelector(".modal-container").style.display = "flex";
+    document.querySelector(".modal").style.display = "block";
+});
+
+document.querySelectorAll(".close").forEach(function(element) {
+    element.addEventListener("click", function() {
+        document.querySelector(".modal-container").style.display = "none";
+        document.querySelector(".modal").style.display = "none";
+    });
+});
+function openModalSell(itemId) {
+    // Set the value of the hidden input field in the form
+    document.getElementById('idinventory').value = itemId;
+
+    // Display the modal
+    document.getElementById('ModalSell').style.display = 'flex';
+  }
+
+  document.getElementById('ModalSell').addEventListener('click', function (e) {
+    if (e.target === this) {
+      this.style.display = 'none';
+    }
   });
 
-document.getElementById("SellForm").addEventListener("submit", function (event) {
-            event.preventDefault();
-            // Получаем форму
-            const form = event.target;
+  document.getElementById("SellForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+    // Получаем форму
+    const form = event.target;
 
-            // Создаем объект FormData для сбора данных из формы
-            const formData = new FormData(form);
+    // Создаем объект FormData для сбора данных из формы
+    const formData = new FormData(form);
 
-            // Здесь вы можете добавить дополнительные действия перед отправкой данных, если необходимо
-
-            // Отправляем данные на сервер с использованием fetch, например
-            fetch("/sell_inventory", {
-                method: "POST",
-                body: formData
-            })
-            .then(response => {
-                // Обработка ответа от сервера
-                if (response.ok) {
-                    //document.getElementById("modalWindow").style.display="none"
-                    location.reload()
-                } else {
-                    alert("Ошибка при обращении к базе")
-                }
-            })
-            .catch(error => {
-                alert("Ошибка при обращении к базе")
-            });
-        });
-        function Sell(id){
-            const Modal = document.getElementById("Modal")
-            Modal.style.display="block"
-            console.log(id)
-            const idinventoryInput = Modal.querySelector('input[name="idinventory"]');
-            idinventoryInput.value = id;
+    // Отправляем данные на сервер с использованием fetch, например
+    fetch("/sell_inventory", {
+        method: "POST",
+        body: formData
+    })
+    .then(response => {
+        // Обработка ответа от сервера
+        if (response.ok) {
+            // Close the modal after successful submission
+            document.getElementById('ModalSell').style.display = 'none';
+            location.reload();  // or perform other actions as needed
+        } else {
+            alert("Ошибка при обращении к базе");
         }
-        function Delete(btnid) {
-            fetch("/del-inventory", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({ id: btnid })
-            })
-            .then(response => {
-                if (response.ok) {
-                    return response.json();
-                } else {
-                    throw new Error("Ошибка HTTP: " + response.status);
-                }
-            })
-            .then(data => {
-                // Обработка данных после успешного запроса
-                console.log("Успешно удалено", data);
-                document.getElementById(btnid).remove();
-            })
-            .catch(error => {
-                console.error("Ошибка при отправке запроса:", error);
-            });
+    })
+    .catch(error => {
+        alert("Ошибка при обращении к базе");
+    });
+  });
+function Sell(id){
+    const Modal = document.getElementById("Modal")
+    Modal.style.display="block"
+    console.log(id)
+    const idinventoryInput = Modal.querySelector('input[name="idinventory"]');
+    idinventoryInput.value = id;
+}
+function Delete(btnid) {
+    fetch("/del-inventory", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ id: btnid })
+    })
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new Error("Ошибка HTTP: " + response.status);
         }
-        document.getElementById("addItemForm").addEventListener("submit", function (event) {
-            event.preventDefault();
+    })
+    .then(data => {
+        // Обработка данных после успешного запроса
+        console.log("Успешно удалено", data);
+        document.getElementById(btnid).remove();
+    })
+    .catch(error => {
+        console.error("Ошибка при отправке запроса:", error);
+    });
+}
+document.getElementById("addItemForm").addEventListener("submit", function (event) {
+    event.preventDefault();
 
-            const name = event.target.name.value;
-            const type = event.target.type.value;
-            const typetext = event.target.type.options[event.target.type.selectedIndex].text;
-            const rented = event.target.rented.checked;
-            const size = event.target.size.value;
+    const name = event.target.name.value;
+    const type = event.target.type.value;
+    const typetext = event.target.type.options[event.target.type.selectedIndex].text;
+    const rented = document.getElementById("rentedCheck").checked
+    const size = document.getElementById("sizeInv").value;
 
-            const table = document.querySelector("table");
-            const newRow = table.insertRow(table.rows.length);
-            const cells = [newRow.insertCell(0), newRow.insertCell(1), newRow.insertCell(2), newRow.insertCell(3), newRow.insertCell(4), newRow.insertCell(5)];
+    const table = document.querySelector("table");
+    const newRow = table.insertRow(table.rows.length);
+    const cells = [
+        newRow.insertCell(0),
+        newRow.insertCell(1),
+        newRow.insertCell(2),
+        newRow.insertCell(3),
+        newRow.insertCell(4),
+        newRow.insertCell(5)
+    ];
 
-            const id = table.rows.length
-            cells[0].innerText = name;
-            cells[1].innerText = typetext;
-            cells[2].innerText = rented;
-            cells[3].innerText = size;
-            var button = document.createElement("button");
-            button.textContent = "Продать"
-            
-            button.onclick = Sell(id)
-            cells[4].appendChild(button)
-            var button = document.createElement("button");
-            button.textContent = "Удалить"
-            button.onclick = Delete(id)
-            cells[5].appendChild(button)
+    const id = table.rows.length;
+    cells[0].innerText = name;
+    cells[1].innerText = typetext;
+    cells[2].innerText = rented;
+    cells[3].innerText = size;
 
-            const formData = new FormData();
-            formData.append("name", name);
-            formData.append("type", type);
-            formData.append("rented", rented);
-            formData.append("size", size);
+    // Создайте кнопку "Продать"
+    var sellButton = document.createElement("button");
+    sellButton.textContent = "Продать";
 
-            fetch("/inventory", {
-                method: "POST",
-                body: formData
-            })
-            .then(response => response.json())
-            .catch(error => {
-                console.error("Ошибка при отправке запроса:", error);
-            });
+    // Назначьте обработчик события для кнопки "Продать"
+    sellButton.onclick = function() {
+        Sell(id);
+    };
 
+    // Добавьте кнопку в ячейку 4
+    cells[4].appendChild(sellButton);
 
-            event.target.reset();
-        });
+    // Создайте кнопку "Удалить"
+    var deleteButton = document.createElement("button");
+    deleteButton.textContent = "Удалить";
+
+    // Назначьте обработчик события для кнопки "Удалить"
+    deleteButton.onclick = function() {
+        Delete(id);
+    };
+
+    // Добавьте кнопку в ячейку 5
+    cells[5].appendChild(deleteButton);
+
+    const formData = new FormData();
+    formData.append("name", name);
+    formData.append("type", type);
+    formData.append("rented", rented);
+    formData.append("size", size);
+
+    fetch("/inventory", {
+        method: "POST",
+        body: formData
+    })
+    .then(response => response.json())
+    .catch(error => {
+        console.error("Ошибка при отправке запроса:", error);
+    });
+
+    event.target.reset();
+    });
+
+    function openModalSell() {
+        document.getElementById('ModalSell').style.display = 'flex';
+      }
+    
+      document.getElementById('ModalSell').addEventListener('click', function (e) {
+        if (e.target === this) {
+          this.style.display = 'none';
+        }
+      });
