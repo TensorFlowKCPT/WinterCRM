ca38f37d0def6646506d875e099bccc090c1e5b7
Vlados <vladik00848@gmail.com>
2023-11-14 18:33:10 +0500

    1

--- st/js/service.js
+++ st/js/service.js
@@ -114,3 +114,79 @@ function UpdatePaymentStatus(id){
     });
     
 }
+function SearchBox() {
+    var input, filter, table, tr, td, i, j, txtValue;
+    input = document.getElementById("serviceSearch");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("Table");
+    tr = table.getElementsByTagName("tr");
+
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("th");
+      for (j = 0; j < td.length; j++) {
+        if (td[j]) {
+          txtValue = td[j].textContent || td[j].innerText;
+          if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            tr[i].style.display = "";
+            break;
+          } else {
+            tr[i].style.display = "none";
+          }
+        }
+      }
+    }
+  }
+
+  function sortTable() {
+    var table = document.getElementById("Table");
+    
+    // Проверка наличия tbody в таблице, если его нет - создаем новый
+    var tbody = table.getElementsByTagName("tbody")[0];
+    if (!tbody) {
+        tbody = document.createElement("tbody");
+        table.appendChild(tbody);
+    }
+
+    var rows = Array.from(tbody.getElementsByTagName("tr"));
+    var selectedValue = document.getElementById("sortirovka-filter").value;
+
+    // Сортировка массива в зависимости от выбранного значения
+    switch (selectedValue) {
+        case "fio":
+            rows.sort(function (a, b) {
+                var nameA = a.getElementsByClassName("content-1")[0].textContent.toUpperCase();
+                var nameB = b.getElementsByClassName("content-1")[0].textContent.toUpperCase();
+                return nameA.localeCompare(nameB);
+            });
+            break;
+        case "date":
+            rows.sort(function (a, b) {
+                var dateA = new Date(a.getElementsByTagName("th")[0].textContent);
+                var dateB = new Date(b.getElementsByTagName("th")[0].textContent);
+                return dateA - dateB;
+            });
+            break;
+        case "inventory":
+            rows.sort(function (a, b) {
+                var inventoryA = a.getElementsByClassName("content-1")[1].textContent.toUpperCase();
+                var inventoryB = b.getElementsByClassName("content-1")[1].textContent.toUpperCase();
+                return inventoryA.localeCompare(inventoryB);
+            });
+            break;
+        case "summ":
+            rows.sort(function (a, b) {
+                var sumA = parseInt(a.getElementsByTagName("th")[5].textContent);
+                var sumB = parseInt(b.getElementsByTagName("th")[5].textContent);
+                return sumA - sumB;
+            });
+            break;
+        default:
+            break;
+    }
+
+    // Переставляем строки в DOM в соответствии с порядком в отсортированном массиве
+    rows.forEach(function (row) {
+        tbody.appendChild(row);
+    });
+}
+
