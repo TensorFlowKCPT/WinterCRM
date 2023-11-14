2dae56d13076b9fb98d282eb7d9f26b1933c0596
Arondhan <lifecrusher7@gmail.com>
2023-11-14 21:46:56 +0500

    employees(modal)

---
+++ st/js/employees.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var modal = document.getElementById('myModal');
+    var openModalBtn = document.getElementById('openModalBtn');
+
+    var closeModalBtn = document.getElementById('closeModalBtn');
+
+    openModalBtn.addEventListener('click', function () {
+        modal.style.display = 'block';
+    });
+
+    closeModalBtn.addEventListener('click', function () {
+        modal.style.display = 'none';
+    });
+
+    window.addEventListener('click', function (event) {
+        if (event.target == modal) {
+            modal.style.display = 'none';
+        }
+    });
+
+    var addItemForm = document.getElementById('addItemForm');
+    addItemForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+    });
+});
