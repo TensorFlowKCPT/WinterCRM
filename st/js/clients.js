159d46f8bcb89030c266fbf49d6ebb90aad027c0
Vlados <vladik00848@gmail.com>
2023-11-30 20:58:28 +0500

    выполнил 2 задания

--- st/js/clients.js
+++ st/js/clients.js
@@ -1,215 +1,233 @@
 function formatClientCount(count) {
-    if (count % 10 === 1 && count % 100 !== 11) {
-        return count + " клиент";
-    } else if (2 <= count % 10 && count % 10 <= 4 && (count % 100 < 10 || count % 100 >= 20)) {
-        return count + " клиента";
-    } else {
-        return count + " клиентов";
-    }
+  if (count % 10 === 1 && count % 100 !== 11) {
+    return count + " клиент";
+  } else if (
+    2 <= count % 10 &&
+    count % 10 <= 4 &&
+    (count % 100 < 10 || count % 100 >= 20)
+  ) {
+    return count + " клиента";
+  } else {
+    return count + " клиентов";
+  }
 }
 
 function openModal() {
-    madal = document.getElementById('myModal');
-    modal.style.display = 'block';
+  madal = document.getElementById("myModal");
+  modal.style.display = "block";
 }
 
 function closeModal() {
-    modal = document.getElementById('myModal');
-    modal.style.display = 'none';
+  modal = document.getElementById("myModal");
+  modal.style.display = "none";
 }
 
 // Функция для обновления массива rows
 function updateRows() {
-    rows = Array.from(tbody.querySelectorAll("tr")).slice(1);
-    return rows
+  rows = Array.from(tbody.querySelectorAll("tr")).slice(1);
+  return rows;
 }
 
-document.getElementById("addItemForm").addEventListener("submit", function (event) {
-            event.preventDefault();
+document
+  .getElementById("addItemForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
 
-            const Fio = event.target.FIO.value;
-            const Passport = event.target.Passport.value;
-            const PhoneNumber = event.target.PhoneNumber.value;
+    const Fio = event.target.FIO.value;
+    const Pledge = event.target.Pledge.value;
+    const Passport = event.target.Passport.value;
+    const PhoneNumber = event.target.PhoneNumber.value;
 
-            const table = document.querySelector("table");
-            const newRow = table.insertRow(table.rows.length);
-            const cells = [newRow.insertCell(0), newRow.insertCell(1), newRow.insertCell(2), newRow.insertCell(3), newRow.insertCell(4)];
+    const table = document.querySelector("table");
+    const newRow = table.insertRow(table.rows.length);
+    const cells = [
+      newRow.insertCell(0),
+      newRow.insertCell(1),
+      newRow.insertCell(2),
+      newRow.insertCell(3),
+      newRow.insertCell(4),
+    ];
 
-            cells[0].classList.add("table-colon");
-            cells[1].classList.add("table-colon");
-            cells[1].classList.add("table-colon2");
-            cells[2].classList.add("table-colon");
-            cells[3].classList.add("table-colon");
-            cells[4].classList.add("table-colon");
+    cells[0].classList.add("table-colon");
+    cells[1].classList.add("table-colon");
+    cells[1].classList.add("table-colon2");
+    cells[2].classList.add("table-colon");
+    cells[3].classList.add("table-colon");
+    cells[4].classList.add("table-colon");
 
-            const button = document.createElement("button");
-            button.classList.add('delete-button')
-            button.innerText = 'Удалить'
-            cells[3].appendChild(button)
+    const button = document.createElement("button");
+    button.classList.add("delete-button");
+    button.innerText = "Удалить";
+    cells[4].appendChild(button);
 
-            const checkbox = document.createElement("input")
-            checkbox.type = 'checkbox'
-            checkbox.classList.add('select-checkbox')
-            cells[4].appendChild(checkbox)
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.classList.add("select-checkbox");
+    cells[4].appendChild(checkbox);
 
-            cells[0].innerText = Fio;
-            cells[1].innerText = Passport;
-            cells[2].innerText = PhoneNumber;
+    cells[0].innerText = Fio;
+    cells[1].innerText = Pledge;
+    cells[2].innerText = Passport;
+    cells[3].innerText = PhoneNumber;
 
-            const formData = new FormData();
-            formData.append("FIO", Fio);
-            formData.append("Passport", Passport);
-            formData.append("PhoneNumber", PhoneNumber);
+    const formData = new FormData();
+    formData.append("FIO", Fio);
+    formData.append("Passport", Passport);
+    formData.append("Pledge", Pledge);
+    formData.append("PhoneNumber", PhoneNumber);
 
-            fetch("/add_client", {
-                method: "POST",
-                body: formData
-            })
-            .then(response => response.json())
-            .then(data => {
-                const newId = data.id
-                button.setAttribute('data-id', newId);
-                checkbox.setAttribute('data-id', newId);
-                newRow.setAttribute('data-id', newId);
+    fetch("/add_client", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        const newId = data.id;
+        button.setAttribute("data-id", newId);
+        checkbox.setAttribute("data-id", newId);
+        newRow.setAttribute("data-id", newId);
 
-                const clientsCountElement = document.getElementById('clientsCount')
-                const currentCount = parseInt(clientsCountElement.textContent, 10)+1;
-                clientsCountElement.textContent = formatClientCount(currentCount)
+        const clientsCountElement = document.getElementById("clientsCount");
+        const currentCount = parseInt(clientsCountElement.textContent, 10) + 1;
+        clientsCountElement.textContent = formatClientCount(currentCount);
+        location.reload();
+      })
+      .catch((error) => {
+        console.error("Ошибка при отправке запроса:", error);
+      });
 
-                var modal = document.getElementById('myModal');
-                closeModal(modal)
-                updateRows()
-            })
-            .catch(error => {
-                console.error("Ошибка при отправке запроса:", error);
-            });
+    event.target.reset();
+  });
 
+var modal = document.getElementById("myModal");
+var openModalBtn = document.getElementById("openModalBtn");
+var closeModalBtn = document.getElementById("closeModalBtn");
 
-            event.target.reset();
-        });
+openModalBtn.addEventListener("click", openModal);
+closeModalBtn.addEventListener("click", closeModal);
 
-
-var modal = document.getElementById('myModal');
-var openModalBtn = document.getElementById('openModalBtn');
-var closeModalBtn = document.getElementById('closeModalBtn');
-
-openModalBtn.addEventListener('click', openModal);
-closeModalBtn.addEventListener('click', closeModal);
-
-
-window.addEventListener('click', function (event) {
-    if (event.target === modal) {
-        closeModal();
-    }
+window.addEventListener("click", function (event) {
+  if (event.target === modal) {
+    closeModal();
+  }
 });
 
-
-window.addEventListener('keydown', function (event) {
-    if (event.key === 'Escape') {
-        closeModal();
-    }
+window.addEventListener("keydown", function (event) {
+  if (event.key === "Escape") {
+    closeModal();
+  }
 });
 
 // Назначить обработчик события на родительский элемент, который существует на момент загрузки страницы
-document.addEventListener('click', function(event) {
-    if (event.target.classList.contains('delete-button')) {
-      const button = event.target;
-      const objectId = button.getAttribute("data-id");
-  
-      fetch(`/del_client`, {
-        method: "DELETE",
-        body: JSON.stringify({ id: objectId }),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      })
-      .then(response => response.json())
-      .then(data => {
+document.addEventListener("click", function (event) {
+  if (event.target.classList.contains("delete-button")) {
+    const button = event.target;
+    const objectId = button.getAttribute("data-id");
+
+    fetch(`/del_client`, {
+      method: "DELETE",
+      body: JSON.stringify({ id: objectId }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
         if (data.success) {
           button.closest("tr").remove();
-  
+
           // Обновить счетчик клиентов
-          const clientsCountElement = document.getElementById('clientsCount');
-          const currentCount = parseInt(clientsCountElement.textContent, 10) - 1;
+          const clientsCountElement = document.getElementById("clientsCount");
+          const currentCount =
+            parseInt(clientsCountElement.textContent, 10) - 1;
           clientsCountElement.textContent = formatClientCount(currentCount);
-          updateRows()
+          updateRows();
         } else {
           console.error("Ошибка удаления объекта.");
         }
       })
-      .catch(error => {
+      .catch((error) => {
         console.error("Ошибка сети: " + error);
       });
-    }
-  });
+  }
+});
 
-  document.addEventListener('click', function(event) {
-    if (event.target && event.target.id === "deleteSelected") {
-      const selectedCheckboxes = document.querySelectorAll(".select-checkbox:checked");
-  
-      if (selectedCheckboxes.length > 0) {
-        const idsToDelete = Array.from(selectedCheckboxes).map(checkbox => checkbox.getAttribute("data-id"));
-  
-        // Отправка запроса на сервер для удаления выбранных объектов
-        fetch("/del_selected_clients", {
-          method: "POST",
-          body: JSON.stringify({ ids: idsToDelete }),
-          headers: {
-            "Content-Type": "application/json"
+document.addEventListener("click", function (event) {
+  if (event.target && event.target.id === "deleteSelected") {
+    const selectedCheckboxes = document.querySelectorAll(
+      ".select-checkbox:checked"
+    );
+
+    if (selectedCheckboxes.length > 0) {
+      const idsToDelete = Array.from(selectedCheckboxes).map((checkbox) =>
+        checkbox.getAttribute("data-id")
+      );
+
+      // Отправка запроса на сервер для удаления выбранных объектов
+      fetch("/del_selected_clients", {
+        method: "POST",
+        body: JSON.stringify({ ids: idsToDelete }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.success) {
+            // Успешно удалено, обновите интерфейс
+            idsToDelete.forEach((id) => {
+              const rowToDelete = document.querySelector(`tr[data-id="${id}"]`);
+              console.log(rowToDelete);
+              if (rowToDelete) {
+                rowToDelete.remove();
+              }
+              updateRows();
+            });
+
+            const clientsCountElement = document.getElementById("clientsCount");
+            const currentCount =
+              parseInt(clientsCountElement.textContent, 10) -
+              idsToDelete.length;
+            clientsCountElement.textContent = formatClientCount(currentCount);
+          } else {
+            console.error("Ошибка удаления объектов.");
           }
         })
-          .then(response => response.json())
-          .then(data => {
-            if (data.success) {
-              // Успешно удалено, обновите интерфейс
-              idsToDelete.forEach(id => {
-                const rowToDelete = document.querySelector(`tr[data-id="${id}"]`);
-                console.log(rowToDelete)
-                if (rowToDelete) {
-                  rowToDelete.remove();
-                }
-                updateRows();
-              });
-  
-              const clientsCountElement = document.getElementById('clientsCount');
-              const currentCount = parseInt(clientsCountElement.textContent, 10) - idsToDelete.length;
-              clientsCountElement.textContent = formatClientCount(currentCount);
-            } else {
-              console.error("Ошибка удаления объектов.");
-            }
-          })
-          .catch(error => {
-            console.error("Ошибка сети: " + error);
-          });
+        .catch((error) => {
+          console.error("Ошибка сети: " + error);
+        });
+    } else {
+      console.log("Нет выбранных объектов для удаления.");
+    }
+  }
+});
+
+document
+  .querySelector(".clients__search-input")
+  .addEventListener("input", function () {
+    var searchInput = this.value.toLowerCase();
+    var rows = document.querySelectorAll("tbody tr");
+
+    for (var i = 0; i < rows.length; i++) {
+      // Начинаем с 1, чтобы пропустить строку с заголовками
+      var cells = rows[i].querySelectorAll(".table-colon");
+      var match = false;
+
+      for (var j = 0; j < cells.length; j++) {
+        var cellContent = cells[j].textContent.toLowerCase();
+        if (cellContent.indexOf(searchInput) !== -1) {
+          match = true;
+          break;
+        }
+      }
+
+      if (match) {
+        rows[i].style.display = "";
       } else {
-        console.log("Нет выбранных объектов для удаления.");
+        rows[i].style.display = "none";
       }
     }
   });
-
-  document.querySelector(".clients__search-input").addEventListener("input", function () {
-    var searchInput = this.value.toLowerCase();
-    var rows = document.querySelectorAll("tbody tr");
-    
-    for (var i = 0; i < rows.length; i++) {  // Начинаем с 1, чтобы пропустить строку с заголовками
-        var cells = rows[i].querySelectorAll(".table-colon");
-        var match = false;
-        
-        for (var j = 0; j < cells.length; j++) {
-            var cellContent = cells[j].textContent.toLowerCase();
-            if (cellContent.indexOf(searchInput) !== -1) {
-                match = true;
-                break;
-            }
-        }
-        
-        if (match) {
-            rows[i].style.display = "";
-        } else {
-            rows[i].style.display = "none";
-        }
-    }
-});
 // СОРТИРОВКА
 function sortTable() {
   var table, rows, switching, i, x, y, shouldSwitch;
@@ -240,8 +258,12 @@ function sortTable() {
     for (i = 1; i < rows.length - 1; i++) {
       shouldSwitch = false;
 
-      x = rows[i].getElementsByTagName("td")[columnIndex].innerText.toLowerCase();
-      y = rows[i + 1].getElementsByTagName("td")[columnIndex].innerText.toLowerCase();
+      x = rows[i]
+        .getElementsByTagName("td")
+        [columnIndex].innerText.toLowerCase();
+      y = rows[i + 1]
+        .getElementsByTagName("td")
+        [columnIndex].innerText.toLowerCase();
 
       if (x > y) {
         shouldSwitch = true;
