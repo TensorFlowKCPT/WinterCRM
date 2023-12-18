908cb7a0037cc7ae54f6157ef4e590fcf0d3e42a
Kirill <m_kirill72@mail.ru>
2023-12-18 17:00:42 +0300

    начал делать календарь

--- st/js/schedule.js
+++ st/js/schedule.js
@@ -201,3 +201,8 @@ const body = document.querySelector("body"),
   toggle.addEventListener("click", () => {
     sidebar.classList.toggle("close");
   })
+
+  function toggleMenu() {
+  var mobileMenu = document.getElementById("mobileMenu");
+  mobileMenu.classList.toggle("show");
+}
